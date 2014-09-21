6a52ab9e929c19a099105f74f01f0776cacf1449
seppel1992 <felixkrautschuk@yahoo.de>
2014-09-21 20:59:57 +0200

    Latitute und Longitude werden angezeigt

--- GPS-App/app/src/main/java/com/example/felix/gps_app/MainActivity.java
+++ GPS-App/app/src/main/java/com/example/felix/gps_app/MainActivity.java
@@ -1,19 +1,52 @@
 package com.example.felix.gps_app;
 
 import android.app.Activity;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 
-public class MainActivity extends Activity
+public class MainActivity extends Activity implements LocationListener
 {
+    LocationManager locationManager;
+    Location location;
+    GoogleMap map;
+    Criteria criteria;
+    String provider;
+    private TextView latituteField;
+    private TextView longitudeField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        latituteField = (TextView) findViewById(R.id.TextViewLatValue);
+        longitudeField = (TextView) findViewById(R.id.TextViewLonValue);
+
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, true);
+        location = locationManager.getLastKnownLocation(provider);
+        locationManager.requestLocationUpdates(provider, 20000, 0, this);
+
+        if(location != null)
+        {
+            onLocationChanged(location);
+        }
     }
 
 
@@ -33,5 +66,52 @@ public class MainActivity extends Activity
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onLocationChanged(Location location)
+    {
+        if(map != null)
+        {
+            drawMarker(location);
+        }
+
+        System.out.println("Provider " + provider + " has been selected.");
+        int lat = (int) (location.getLatitude());
+        int lng = (int) (location.getLongitude());
+        latituteField.setText(String.valueOf(lat));
+        longitudeField.setText(String.valueOf(lng));
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle)
+    {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s)
+    {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s)
+    {
+
+    }
+
+    private void drawMarker(Location location)
+    {
+        LatLng currentPosition;
+
+        map.clear();
+        currentPosition= new LatLng(location.getLatitude(), location.getLongitude());
+
+        //Zoom zur aktuellen Position
+        map.animateCamera(CameraUpdateFactory.newLatLng(currentPosition));
+
+        //aktuelle Position markieren
+        map.addMarker(new MarkerOptions().position(currentPosition).snippet("Lat: " + location.getLatitude() + "Lng: " + location.getLongitude()));
     }
 }
